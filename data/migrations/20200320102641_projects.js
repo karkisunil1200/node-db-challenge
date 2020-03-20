e514ff3d554ae08e2e6c113fcd90c8ec68efae4a
Sunil Karki <karkisunil1200@gmail.com>
2020-03-20 11:07:54 -0500

    Added migrations

--- data/migrations/20200320102641_projects.js
+++ data/migrations/20200320102641_projects.js
@@ -1,8 +1,75 @@
-
 exports.up = function(knex) {
-  
+  return (
+    knex.schema
+      //Projects
+      .createTable('projects', tbl => {
+        tbl.increments();
+
+        tbl
+          .string('project_name')
+          .notNullable()
+          .unique();
+
+        tbl.string('description');
+      })
+
+      //Tasks
+      .createTable('tasks', tbl => {
+        tbl.increments();
+
+        tbl
+          .integer('project_id')
+          .notNullable()
+          .references('id')
+          .inTable('projects');
+
+        tbl
+          .string('name')
+          .notNullable()
+          .unique();
+
+        tbl.string('description').notNullable();
+        tbl.string('notes').notNullable();
+        tbl.boolean('completed').notNullable();
+      })
+      //Resources
+      .createTable('resources', tbl => {
+        tbl.increments();
+
+        tbl
+          .string('name')
+          .notNullable()
+          .unique();
+
+        tbl.string('description').notNullable();
+        tbl.string('notes');
+      })
+
+      //Connection
+      .createTable('connection', tbl => {
+        tbl
+          .integer('project_id')
+          .notNullable()
+          .references('id')
+          .inTable('projects');
+        tbl
+          .integer('task_id')
+          .notNullable()
+          .references('id')
+          .inTable('tasks');
+        tbl
+          .integer('resource_id')
+          .notNullable()
+          .reference('id')
+          .inTable('resources');
+      })
+  );
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('connection')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('projects');
 };
