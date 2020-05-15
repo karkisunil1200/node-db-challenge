8f30384102874edefee53a76d1f511627d9b5436
Sunil Karki <karkisunil1200@gmail.com>
2020-05-15 11:25:11 -0500

    Added seeds file

--- data/migrations/20200417104653_tasks.js
+++ data/migrations/20200417104653_tasks.js
@@ -2,13 +2,12 @@ exports.up = function(knex) {
   return knex.schema.createTable('tasks', tbl => {
     tbl.increments();
 
-    tbl.string('description');
+    tbl.string('description').notNullable();
     tbl.string('notes');
     tbl.bool('completed').defaultTo(false);
 
     tbl
       .integer('project_id')
-      .notNullable()
       .references('id')
       .inTable('projects')
       .onDelete('RESTRICT')
--- projects/project-router.js
+++ projects/project-router.js
@@ -13,6 +13,22 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+
+  Projects.findProject(id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({message: 'Id not available'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: err.messasge});
+    });
+});
+
 router.post('/', (req, res) => {
   const data = req.body;
 
